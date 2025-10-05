ffafffc6671ed0fd0977d2b8607d927ce36ba37a
Nathan Pithie <nathan.pithie@gmail.com>
2025-10-05 16:00:53 +1000

    Added blueprints for templates

--- club95/views.py
+++ club95/views.py
@@ -2,6 +2,22 @@ from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__)
 
+# Home page
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+# Event details
+@main_bp.route('/eventdetails.html')
+def eventdetails():
+    return render_template('eventdetails.html')
+
+# Create event
+@main_bp.route('/createevent.html')
+def createevent():
+    return render_template('createevent.html')
+
+# My tickets
+@main_bp.route('/mytickets.html')
+def mytickets():
+    return render_template('mytickets.html')
