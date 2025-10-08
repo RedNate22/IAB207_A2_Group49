f9f89e517f8d3a37ce0871b8f836f76c2135da02
Nathan Pithie <nathan.pithie@gmail.com>
2025-10-08 17:12:21 +1000

    Removed duplicate import for user blueprint

--- club95/__init__.py
+++ club95/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask, session
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from .user import user_bp
 
 db = SQLAlchemy()
 
