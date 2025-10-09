653ce58e1c55d66737828ae50337fa9bc92d9692
BrynMcCulloch <brynmcc00@gmail.com>
2025-10-10 09:53:29 +1000

    added phone number field to sign up form

--- club95/form.py
+++ club95/form.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 #Sets up forms for use thorughout the application using Flask-WTF and WTForms
 #https://wtforms.readthedocs.io/en/3.2.x/forms/ - Good read on WTForms
@@ -13,11 +13,12 @@ class LoginForm(FlaskForm):
     name = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
-# adds forms for registration
+# adds forms for registration. Note that 15 is the max length for a phone number
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     name = StringField('Name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    phonenumber = StringField('Phone No.', [Length(min=4, max=25)])
     submit = SubmitField('Register')
 # adds forms for creating events
 class EventForm(FlaskForm):
