b7c96fc3501def9ae5014c0276639e5586c0421d
Nathan Pithie <nathan.pithie@gmail.com>
2025-10-10 13:57:05 +1000

    Changed 'subtitle' to 'heading' for compatibility with other code

--- club95/user.py
+++ club95/user.py
@@ -5,7 +5,7 @@ user_bp = Blueprint('user_bp', __name__, template_folder='templates')
 # My tickets page
 @user_bp.route('/user/mytickets')
 def mytickets():
-    return render_template('user/mytickets.html', subtitle='My Tickets')
+    return render_template('user/mytickets.html', heading='My Tickets')
 
 # ! NOT IMPLEMENTED
 # User profile page
