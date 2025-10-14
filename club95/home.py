4cad29fabafcf7acb264d6564a692b77524d3fcd
BrynMcCulloch <brynmcc00@gmail.com>
2025-10-15 09:59:50 +1000

    added search function to home.py and attached it to search form on index.html

--- club95/home.py
+++ club95/home.py
@@ -1,8 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Event
+from . import db
 
 home_bp = Blueprint('home_bp', __name__, template_folder='templates')
 
 # Home page
 @home_bp.route('/')
 def index():
-    return render_template('index.html', heading='Browse Events')
+    event = db.session.scalars(db.select(Event)).all()
+    return render_template('index.html', heading='Browse Events', events=event)
+
+@home_bp.route('/search')
+def search():
+    if request.args.get('search') and request.args.get('search') != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        events = db.session.scalars(db.select(Event).where(Event.title.like(query))).all()
+        return render_template('index.html', heading='Browse Events', events=events)
+    else:
+        return redirect(url_for('home_bp.index'))
